fa845e6394e69c25bc99b727303e00b8cabccbd2
lzc <1436899024@qq.com>
2019-01-12 19:29:53 +0800

    Initial commit

---
+++ src/main/java/com/lzc/teachingInteraction/TeachinginteractionApplication.java
@@ -0,0 +1,14 @@
+package com.lzc.teachingInteraction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeachinginteractionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeachinginteractionApplication.class, args);
+    }
+
+}
+
